ad10d8dca6d8337e0517d9dc5f794d71b1909d3e
Faizan-92 <16bit051@nirmauni.ac.in>
2020-05-13 18:14:44 +0530

    Initial commit

---
+++ app/src/main/java/com/android/iunoob/bloodbank/viewmodels/DownloadUrl.java
@@ -0,0 +1,50 @@
+package com.android.iunoob.bloodbank.viewmodels;
+
+import android.util.Log;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class DownloadUrl {
+
+    public String readUrl(String myUrl)
+    {
+        String data = "";
+        InputStream inputStream = null;
+        HttpURLConnection urlConnection = null;
+        try {
+            try {
+                URL url = new URL(myUrl);
+                urlConnection = (HttpURLConnection) url.openConnection();
+                urlConnection.connect();
+                urlConnection.setRequestMethod("GET");
+                inputStream = urlConnection.getInputStream();
+                BufferedReader br = new BufferedReader(new InputStreamReader(inputStream));
+                StringBuffer sb = new StringBuffer();
+                String line = "";
+                while ((line = br.readLine()) != null) {
+                    sb.append(line);
+                }
+
+                data = sb.toString();
+                br.close();
+
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (Exception e) {
+                e.printStackTrace();
+            } finally {
+                inputStream.close();
+                urlConnection.disconnect();
+            }
+        }catch (Exception e){
+            Log.d("NearbyHospital","Caught exception e:"+e.getMessage());
+        }
+        return data;
+    }
+}
